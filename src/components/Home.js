3940a5c85370764c476f16bdab16189711dd4833
Akshay More <moreakshay725@gmail.com>
2021-08-02 21:48:27 +0530

    to the moon

---
+++ src/components/Home.js
@@ -0,0 +1,145 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <div class="max-w-md mx-auto my-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.pexels.com/photos/1804514/pexels-photo-1804514.jpeg?auto=compress"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              philosophy
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              A Look into Life of Aristotle
+            </a>
+            <p class="mt-2 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam iusto quam dolores nulla accusamus ab est
+            </p>
+          </div>
+        </div>
+      </div>
+      {/*2nd  */}
+      <div class="max-w-md mx-auto my-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.pexels.com/photos/4546022/pexels-photo-4546022.jpeg?auto=compress"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Education
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              Understanding Privatization and Its Impact
+            </a>
+            <p class="mt-2 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam iusto quam dolores nulla accusamus ab est
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 3rd */}
+      <div class="max-w-md mx-auto my-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.pexels.com/photos/5668473/pexels-photo-5668473.jpeg?auto=compress"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              Government
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              An Introduction to the Government in India
+            </a>
+            <p class="mt-2 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam iusto quam dolores nulla accusamus ab est
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* 4th */}
+      <div class="max-w-md mx-auto my-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.pexels.com/photos/1112048/pexels-photo-1112048.jpeg?auto=compress"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              religious studies
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              A Research on the Terms and Descriptions for Satanism
+            </a>
+            <p class="mt-2 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam iusto quam dolores nulla accusamus ab est
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* 5th */}
+      <div class="max-w-md mx-auto my-8 bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+          <div class="md:flex-shrink-0">
+            <img
+              class="h-48 w-full object-cover md:h-full md:w-48"
+              src="https://images.pexels.com/photos/4101143/pexels-photo-4101143.jpeg?auto=compress"
+              alt="Man looking at item at a store"
+            />
+          </div>
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+              psychology and psychiatry
+            </div>
+            <a
+              href="#"
+              class="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+            >
+              An Analysis of the Self-Awareness of Values and Biases
+            </a>
+            <p class="mt-2 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Laboriosam iusto quam dolores nulla accusamus ab est
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
